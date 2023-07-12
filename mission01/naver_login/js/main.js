a4f49945fbab624fd8cd2198dab04ea221db4a78
ingbin-git <eeeingbin@gmail.com>
2023-07-13 03:25:40 +0900

    사용자 로그인 인증 - function - if문 - querySelector - onclick

--- mission01/naver_login/js/main.js
+++ mission01/naver_login/js/main.js
@@ -1,36 +1,40 @@
-
 const user = {
-  id:'asd@naver.com',
-  pw:'spdlqj123!@'
-}
+  id: 'asd@naver.com',
+  pw: 'spdlqj123!@',
+};
 
-/*
+document.querySelector('input.user-password-input').onclick =
+  function enterId() {
+    const inputId = document.querySelector('input.user-email-input');
+    if (!emailReg(inputId.value)) {
+      inputId.classList.add('is--invalid');
+    } else if (inputId.value !== user.id) {
+      alert('아이디가 일치하지 않습니다.');
+    } else {
+      inputId.classList.remove('is--invalid');
+    }
+  };
 
-1. email 정규표현식을 사용한 validation
-2. pw 정규표현식을 사용한 validation
-3. 상태 변수 관리
-4. 로그인 버튼을 클릭시 조건처리
+document.querySelector('button.btn-login').onclick = function enterPw() {
+  const inputPw = document.querySelector('input.user-password-input');
+  if (!pwReg(inputPw.value)) {
+    inputPw.classList.add('is--invalid');
+  } else if (inputPw.value !== user.pw) {
+    alert('비밀번호가 일치하지 않습니다.');
+  } else {
+    inputPw.classList.remove('is--invalid');
+    window.location.href = 'welcome.html';
+  }
+};
 
-*/
+function emailReg(text) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-function emailReg(text){
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-  return re.test(String(text).toLowerCase())
-}
-
-function pwReg(text){
-  const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
   return re.test(String(text).toLowerCase());
 }
 
-
-
-
-
-
-
-
-
-
-
+function pwReg(text) {
+  const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
+  return re.test(String(text).toLowerCase());
+}
