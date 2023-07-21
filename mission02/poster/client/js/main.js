57a38ef937eff68d7b8a7efa9f225ad4a46c622f
ingbin-git <eeeingbin@gmail.com>
2023-07-21 23:16:12 +0900

    과제 관련 주석 삭제

--- mission02/poster/client/js/main.js
+++ mission02/poster/client/js/main.js
@@ -1,13 +1,3 @@
-/* 
-
-1. 클릭 이벤트 활성화
-2. nav 클릭시 배경 색상 변경
-3. 이미지 변경
-4. 텍스트 변경
-5. 함수 분리
-
-*/
-
 const body = getNode('body');
 const nickName = getNode('.nickName');
 const navigation = getNode('.nav');
